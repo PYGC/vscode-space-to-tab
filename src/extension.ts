62e75e6967943c9b06a912b548b0b9183e7f292b
Kismet Liu <850345193@qq.com>
2021-11-23 14:12:14 +0800

    Fix bugs

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,27 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+// Get string length
+function getLen(str :string) :number {
+    let length = 0;
+    for(let i=0; i<str.length; i++) {
+        let chr = str.charCodeAt(i);
+        if( (chr >= 0x00 && chr <= 0x80) ||
+            (chr >= 0xa0 && chr <= 0xff) ||
+            (chr === 0xf8f0) ||
+            (chr >= 0xff61 && chr <= 0xff9f) ||
+            (chr >= 0xf8f1 && chr <= 0xf8f3)){
+            // Half-angle character
+            length += 1;
+        }else{
+            // Full-angle character
+            length += 2;
+        }
+    }
+
+    return length;
+};
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -20,24 +41,34 @@ export function activate(context: vscode.ExtensionContext) {
     }
 
     registerCommandNice('spacetab.convert', () => {
-        // エディタ取得
         const editor = vscode.window.activeTextEditor as vscode.TextEditor;
-        // ドキュメント取得
         const doc = editor.document;
-        // ドキュメント全てを取得する
         const all_selection = new vscode.Selection(
             new vscode.Position(0, 0),
             new vscode.Position(doc.lineCount - 1, 10000));
 
         let tabSize = editor.options.tabSize as number;
+
         editor.edit(edit => {
             for (let i = 0; i < doc.lineCount; i++) {
+                let text = doc.lineAt(i).text;
+                
+                // First, tab to space
+                while (text.indexOf('\t') != -1) {
+                    let position = text.indexOf('\t');
+                    let before = text.substring(0, position);
+                    let beforeLen = getLen(before);
+                    let spacenum = tabSize - (beforeLen % tabSize);
+                    let texttemp = before + ' '.repeat(spacenum) + text.substring(position + 1);
+                    text = texttemp;
+                }
+                
                 let reString = `.{1,${tabSize}}`;
                 let re = new RegExp(reString, 'g');
-                let text = doc.lineAt(i).text;
                 let textSplit = text.match(re);
                 let textNew = "";
 
+                // Second, space to tab
                 while (textSplit !== null && textSplit.length > 0) {
                     let chars = textSplit.shift();
                     if (chars !== undefined) {
