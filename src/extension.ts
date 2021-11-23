085c3221dbf65f42db6c28174cce57ec06bb543e
Kismet Liu <850345193@qq.com>
2021-11-23 14:42:11 +0800

    Format ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // Get string length
@@ -23,18 +21,7 @@ function getLen(str :string) :number {
     return length;
 };
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    //console.log('Congratulations, your extension "markdowntable" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-
     function registerCommandNice(commandId: string, run: (...args: any[]) => void): void {
         let command = vscode.commands.registerCommand(commandId, run);
         context.subscriptions.push(command);
@@ -54,7 +41,7 @@ export function activate(context: vscode.ExtensionContext) {
                 let text = doc.lineAt(i).text;
                 
                 // First, tab to space
-                while (text.indexOf('\t') != -1) {
+                while (text.indexOf('\t') !== -1) {
                     let position = text.indexOf('\t');
                     let before = text.substring(0, position);
                     let beforeLen = getLen(before);
